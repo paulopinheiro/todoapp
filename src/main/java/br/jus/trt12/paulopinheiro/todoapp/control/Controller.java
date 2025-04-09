6c4cc1246ca88e812b83dfd523e3fbc2b6ec1b45
Paulo Pinheiro <pinus.sc@gmail.com>
2025-04-09 18:40:03 -0300

    Project Setup and Login Screen - Part 1

---
+++ src/main/java/br/jus/trt12/paulopinheiro/todoapp/control/Controller.java
@@ -0,0 +1,13 @@
+package br.jus.trt12.paulopinheiro.todoapp.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+public class Controller implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
