60862bb70f9025b32e955441cc605ec5dd30a296
Paulo Pinheiro <pinus.sc@gmail.com>
2025-04-10 14:23:04 -0300

    Add Item Screen - Part 2

---
+++ src/main/java/br/jus/trt12/paulopinheiro/todoapp/control/AddItemController.java
@@ -0,0 +1,18 @@
+package br.jus.trt12.paulopinheiro.todoapp.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+
+public class AddItemController implements Initializable {
+    @FXML
+    private ImageView addButton;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
