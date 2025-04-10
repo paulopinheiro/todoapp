4cc077b52af66f3715b49ee8fdc02f43d038a1a2
Paulo Pinheiro <pinus.sc@gmail.com>
2025-04-10 14:17:20 -0300

    Some improvements do login.fxml

---
+++ src/main/java/br/jus/trt12/paulopinheiro/todoapp/control/LoginController.java
@@ -0,0 +1,29 @@
+package br.jus.trt12.paulopinheiro.todoapp.control;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginController implements Initializable {
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    private PasswordField loginPassword;
+
+    @FXML
+    private Button loginSignupButton;
+
+    @FXML
+    private TextField loginUsername;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
