af260ef0d045ffaf33a9bd0d69431a181dd80de6
Paulo Pinheiro <pinus.sc@gmail.com>
2025-04-11 13:05:16 -0300

    Setup User Model Class - Part 8

--- src/main/java/br/jus/trt12/paulopinheiro/todoapp/control/SignupController.java
+++ src/main/java/br/jus/trt12/paulopinheiro/todoapp/control/SignupController.java
@@ -1,6 +1,7 @@
 package br.jus.trt12.paulopinheiro.todoapp.control;
 
 import br.jus.trt12.paulopinheiro.todoapp.database.DatabaseHandler;
+import br.jus.trt12.paulopinheiro.todoapp.model.User;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
@@ -13,25 +14,18 @@ import javafx.scene.control.TextField;
 public class SignupController implements Initializable {
     @FXML
     private Button signUpButton;
-
     @FXML
     private CheckBox signUpCheckBoxFemale;
-
     @FXML
     private CheckBox signUpCheckBoxMale;
-
     @FXML
     private TextField signUpFirstName;
-
     @FXML
     private TextField signUpLastName;
-
     @FXML
     private TextField signUpLocation;
-
     @FXML
     private PasswordField signUpPassword;
-
     @FXML
     private TextField signUpUsername;
 
@@ -40,13 +34,32 @@ public class SignupController implements Initializable {
         DatabaseHandler databaseHandler = new DatabaseHandler();
 
         signUpButton.setOnAction(event-> {
-            databaseHandler.signUpUser(signUpFirstName.getText(),
-                                       signUpLastName.getText(),
-                                       signUpUsername.getText(),
-                                       signUpPassword.getText(),
-                                       signUpLocation.getText(),
-                                       "Male");
+            databaseHandler.signUpUser(getUser());
         });
-    }    
-    
+    }
+
+    private User getUser() {
+        User user = new User(signUpFirstName.getText(),
+                             signUpLastName.getText(),
+                             signUpUsername.getText(),
+                             signUpPassword.getText(),
+                             signUpLocation.getText(),
+                             getGender());
+        return user;
+    }
+
+    private String getGender() {
+        String gender = "Undefined";
+
+        if (signUpCheckBoxMale.isSelected()) {
+            if (!this.signUpCheckBoxFemale.isSelected()) {
+                gender = "Male";
+            }
+        } else {
+            if (signUpCheckBoxFemale.isSelected()) {
+                gender = "Female";
+            }
+        }
+        return gender;
+    }
 }
--- src/main/java/br/jus/trt12/paulopinheiro/todoapp/database/DatabaseHandler.java
+++ src/main/java/br/jus/trt12/paulopinheiro/todoapp/database/DatabaseHandler.java
@@ -1,5 +1,6 @@
 package br.jus.trt12.paulopinheiro.todoapp.database;
 
+import br.jus.trt12.paulopinheiro.todoapp.model.User;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -9,14 +10,15 @@ import java.util.logging.Logger;
 
 public class DatabaseHandler extends Configs {
     private Connection dbConnection;
+    private final String connectionURL = "jdbc:derby://" + dbHost + ":" + dbPort + "/" + dbName;
 
-    public Connection getDbConnection() throws ClassNotFoundException, SQLException {
-        String connectionURL = "jdbc:derby://" + dbHost + ":" + dbPort + "/" + dbName;
-        dbConnection = DriverManager.getConnection(connectionURL,dbUser, dbPass);
+    private Connection getDbConnection() throws ClassNotFoundException, SQLException {
+        if (dbConnection==null)
+            dbConnection = DriverManager.getConnection(connectionURL,dbUser, dbPass);
         return dbConnection;
     }
 
-    public void signUpUser(String firstname, String lastname, String username, String password, String location, String gender) {
+    public void signUpUser(User user) {
         try {
             String insert = "INSERT INTO " + Const.USERS_TABLE + "("
                     + Const.USERS_FIRSTNAME + ", "
@@ -26,16 +28,39 @@ public class DatabaseHandler extends Configs {
                     + Const.USERS_LOCATION + ", "
                     + Const.USERS_GENDER + ") VALUES(?,?,?,?,?,?)";
             PreparedStatement preparedStatement = getDbConnection().prepareStatement(insert);
-            preparedStatement.setString(1, firstname);
-            preparedStatement.setString(2, lastname);
-            preparedStatement.setString(3, username);
-            preparedStatement.setString(4, password);
-            preparedStatement.setString(5, location);
-            preparedStatement.setString(6, gender);
+            preparedStatement.setString(1, user.getFirstName());
+            preparedStatement.setString(2, user.getLastName());
+            preparedStatement.setString(3, user.getUserName());
+            preparedStatement.setString(4, user.getPassword());
+            preparedStatement.setString(5, user.getLocation());
+            preparedStatement.setString(6, user.getGender());
 
             preparedStatement.executeUpdate();
         } catch (SQLException | ClassNotFoundException ex) {
             Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+//
+//    public void signUpUser(String firstname, String lastname, String username, String password, String location, String gender) {
+//        try {
+//            String insert = "INSERT INTO " + Const.USERS_TABLE + "("
+//                    + Const.USERS_FIRSTNAME + ", "
+//                    + Const.USERS_LASTNAME + ", "
+//                    + Const.USERS_USERNAME + ", "
+//                    + Const.USERS_PASSWORD + ", "
+//                    + Const.USERS_LOCATION + ", "
+//                    + Const.USERS_GENDER + ") VALUES(?,?,?,?,?,?)";
+//            PreparedStatement preparedStatement = getDbConnection().prepareStatement(insert);
+//            preparedStatement.setString(1, firstname);
+//            preparedStatement.setString(2, lastname);
+//            preparedStatement.setString(3, username);
+//            preparedStatement.setString(4, password);
+//            preparedStatement.setString(5, location);
+//            preparedStatement.setString(6, gender);
+//
+//            preparedStatement.executeUpdate();
+//        } catch (SQLException | ClassNotFoundException ex) {
+//            Logger.getLogger(DatabaseHandler.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
 }
